597fde95d2eb612f96656ddc7722bd8316f8499b
vrushabh95 <mundhe.vrushabh@gmail.com>
2021-03-03 10:27:34 +0530

    ADDED : First Name And Last Name In Reverse Order

---
+++ BasicPythonProgram/Name.py
@@ -0,0 +1,13 @@
+def reverseString(fname,lname):
+    firstNameReverse=''.join(reversed(fname))
+    lastNameReverse=''.join(reversed(lname))
+    fullName=firstNameReverse +" "+lastNameReverse
+    return fullName
+
+if __name__ == "__main__":
+    firstName = input("Enter the first Name ")
+    lastName = input("Enter the Last Name ")
+    print("Before reversing")
+    print(firstName+" "+lastName)
+    print("After reversing")
+    print(reverseString(firstName,lastName))
